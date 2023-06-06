f991ba8bd8de7b64c7bcced865632848e58870f3
karthika RD <105119562+Karthikard@users.noreply.github.com>
2023-06-06 10:27:43 +0530

    Update class_methods.java

--- class_methods.java
+++ class_methods.java
@@ -1,3 +1,5 @@
+//circle class
+
 public class Main
 {
 	public static void main(String[] args) {
@@ -20,5 +22,42 @@ class circle
     {
         a2=2*Math.PI*r2;
         System.out.println(a2);
+    }
+}
+
+
+//rectangle class
+public class Main
+{
+	public static void main(String[] args) {
+		rectangle r1=new rectangle();
+		r1.area(10,20);
+		r1.perimeter(10,20);
+		System.out.println(r1.issquare(10,20));
+	}
+}
+class rectangle
+{
+    int area,perimeter;
+    public void area(int l,int b)
+    {
+        area=l*b;
+        System.out.println(area);
+    }
+    public void perimeter(int l,int b)
+    {
+        perimeter=2*l+2*b;
+        System.out.println(perimeter);
+    }
+    public boolean issquare(int l,int b)
+    {
+        if(l==b)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
