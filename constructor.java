564dd88212b8ecd6815259fd8e6042777230800b
karthika RD <105119562+Karthikard@users.noreply.github.com>
2023-06-06 15:13:47 +0530

    Create constructor.java

---
+++ constructor.java
@@ -0,0 +1,6 @@
+/* purpose : Initializing property while constructing the object itself. 
+These constructors are automatically called when object is created.
+Constructor do not have return type.
+2 types : non paramaterized and parameterized contructor
+constructor overloading is possible
+*/
