b0e694e0e5d38b79a80824529b50366758717f26
karthika RD <105119562+Karthikard@users.noreply.github.com>
2023-06-24 21:19:43 +0530

    Create abstract.java

---
+++ abstract.java
@@ -0,0 +1,5 @@
+/* There are two kinds of class : Abstract class and concrete class .
+  Abstract class contains abstract keyword . ex: abstract class rectangle.
+  when a method is created as abstract then the class must be abstract.
+  when a class is derived from abstract class , then the derived class will also become abstract class.To avoid these overriding of method must take place.
+*/
