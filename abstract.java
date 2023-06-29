0ede2654e21722b954a028738e9bd39223253148
karthika RD <105119562+Karthikard@users.noreply.github.com>
2023-06-29 21:20:06 +0530

    Update abstract.java

--- abstract.java
+++ abstract.java
@@ -3,3 +3,15 @@
   when a method is created as abstract then the class must be abstract.
   when a class is derived from abstract class , then the derived class will also become abstract class.To avoid these overriding of method must take place.
 */
+abstract class main1
+{
+  abstract void num(){};
+}
+class main2 extands main1
+{
+  void num()
+  {
+    psvm("welcome");
+  }
+}
+  
