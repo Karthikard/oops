6a9037d577e176b2b313277b8b02369c0e2bf250
karthika RD <105119562+Karthikard@users.noreply.github.com>
2023-07-02 14:16:26 +0530

    Create innerclass1.java

---
+++ innerclass1.java
@@ -0,0 +1,36 @@
+/*
+To reduce the complexity of large class ,inner class are used.
+Inner class can access the members of outer class.
+Objects for inner class must be created inside the outer class.
+Outer class cannot use the members of inner class directly unless it has created the object.
+
+*/
+
+class Main 
+{
+    public static void main (String[] args) {
+       outer o=new outer();
+       o.outerdisplay();
+    }
+}
+class outer 
+{
+    int x=10;
+    class inner 
+    {
+        int y=20;
+        public void innerdisplay()
+        {
+            System.out.println(x+"  "+y);
+        }
+    }
+    public void outerdisplay()
+    {
+        inner i=new inner();
+        i.innerdisplay();
+        
+    }
+}
+/* output
+  10  20
+*/
